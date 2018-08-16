33c257bbb24bb72856b7b932566c1c281a821c50
Vitaly Zolkin <al-not@ya.ru>
2018-08-16 13:34:05 +0500

    add preferences

--- app/src/main/java/online/z0lk1n/android/instagram_lite/MainActivity.java
+++ app/src/main/java/online/z0lk1n/android/instagram_lite/MainActivity.java
@@ -1,13 +1,58 @@
 package online.z0lk1n.android.instagram_lite;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MIN_TEXT_LENGTH = 10;
+    private TextInputLayout mTextInputLayout;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextInputLayout = findViewById(R.id.textInputLayout);
+        mEditText = findViewById(R.id.editText);
+
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (shouldShowError()) {
+                    showError();
+                } else {
+                    hideError();
+                }
+            }
+        });
+    }
+
+    private boolean shouldShowError() {
+        int textLength = mEditText.getText().length();
+        return textLength > 0 && textLength < MIN_TEXT_LENGTH;
+    }
+
+    private void showError() {
+        mTextInputLayout.setError(getString(R.string.error_input_text)
+                + MIN_TEXT_LENGTH);
+    }
+
+    private void hideError() {
+        mTextInputLayout.setError(null);
     }
 }
--- app/src/main/java/online/z0lk1n/android/instagram_lite/SettingsActivity.java
+++ app/src/main/java/online/z0lk1n/android/instagram_lite/SettingsActivity.java
@@ -1,6 +1,21 @@
 package online.z0lk1n.android.instagram_lite;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.annotation.Nullable;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+
+    }
 }
