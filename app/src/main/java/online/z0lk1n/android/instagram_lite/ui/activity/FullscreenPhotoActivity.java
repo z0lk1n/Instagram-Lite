994c0d850e584c883c0db8488279ea7ef0de944e
Vitaly Zolkin <al-not@ya.ru>
2018-09-10 17:01:15 +0500

    fix theme FullscreenPhotoActivity

--- app/src/main/java/online/z0lk1n/android/instagram_lite/ui/activity/FullscreenPhotoActivity.java
+++ app/src/main/java/online/z0lk1n/android/instagram_lite/ui/activity/FullscreenPhotoActivity.java
@@ -1,16 +1,13 @@
 package online.z0lk1n.android.instagram_lite.ui.activity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.media.ExifInterface;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
@@ -18,46 +15,29 @@ import online.z0lk1n.android.instagram_lite.R;
 import online.z0lk1n.android.instagram_lite.util.Const;
 import online.z0lk1n.android.instagram_lite.util.PhotoManager;
 
-public class FullscreenPhotoActivity extends BaseActivity {
+public final class FullscreenPhotoActivity extends AppCompatActivity {
 
     public static final String NAME = "b9e6ee8c-3f43-457d-ad77-4d99891ef7bc";
     private static final String TAG = "FullscreenPhotoActivity";
 
-    private static final boolean AUTO_HIDE = true;
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler hideHandler = new Handler();
+    private final Runnable hideRunnable = this::hide;
     private ImageView imageView;
+    private boolean isVisible;
 
-    private final Runnable hidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
+    private final Runnable hidePart2Runnable = () ->
             imageView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private final Runnable mShowPart2Runnable = () -> {
+
+    private final Runnable showPart2Runnable = () -> {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.show();
-        }
-    };
-
-    private boolean mVisible;
-    private final Runnable mHideRunnable = () -> hide();
-
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
         }
     };
 
@@ -70,43 +50,47 @@ public class FullscreenPhotoActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setDisplayShowHomeEnabled(true);
         }
 
-        mVisible = true;
+        isVisible = true;
         imageView = findViewById(R.id.fullscreen_photo);
 
         int width = PhotoManager.calculateWidthOfPhoto(this, 1);
         int height = PhotoManager.calculateHeightOfPhoto(this);
-
         File file = new File(getIntent().getStringExtra(Const.KEY_FULLSCREEN_PHOTO));
-
         switch (PhotoManager.getOrientationPhoto(file.getPath())) {
             case ExifInterface.ORIENTATION_NORMAL:
-                Picasso.get().load(file).resize(width, 0).into(imageView);
+                PhotoManager.setPhoto(imageView, file, width, 0);
                 break;
             case ExifInterface.ORIENTATION_ROTATE_90:
-                Picasso.get().load(file).resize(0, height).into(imageView);
+                PhotoManager.setPhoto(imageView, file, 0, height);
                 break;
             default:
-                Picasso.get().load(file).resize(width, width).into(imageView);
+                PhotoManager.setPhoto(imageView, file, width, width);
                 break;
         }
 
         imageView.setOnClickListener(view -> toggle());
     }
 
-
-
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        delayedHide(100);
+        hideHandler.removeCallbacks(hideRunnable);
+        hideHandler.postDelayed(hideRunnable, UI_ANIMATION_DELAY);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
     private void toggle() {
-        if (mVisible) {
+        if (isVisible) {
             hide();
         } else {
             show();
@@ -118,30 +102,16 @@ public class FullscreenPhotoActivity extends BaseActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-        mVisible = false;
-
-        hideHandler.removeCallbacks(mShowPart2Runnable);
+        isVisible = false;
+        hideHandler.removeCallbacks(showPart2Runnable);
         hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    @SuppressLint("InlinedApi")
     private void show() {
         imageView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
+        isVisible = true;
         hideHandler.removeCallbacks(hidePart2Runnable);
-        hideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private void delayedHide(int delayMillis) {
-        hideHandler.removeCallbacks(mHideRunnable);
-        hideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
+        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY);
     }
 }
